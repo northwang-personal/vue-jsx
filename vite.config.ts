0cf6b3e592a9ac3e4ac13cc2b71f42a8f5778a2f
northwang <69568203+northwang-lucky@users.noreply.github.com>
2023-01-05 22:24:58 +0800

    chore: Added browser compatibility configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,18 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue({ reactivityTransform: true }), vueJsx()],
+  plugins: [
+    vue({ reactivityTransform: true }),
+    vueJsx(),
+    legacy({
+      targets: ['> 1%', 'last 2 versions'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
